6a6c88a78bb412a7b78cb7c6bfee4d8adb532971
Hugh Jin <hughjin@Hughs-MacBook-Pro.local>
2023-07-29 08:24:22 -0700

    Adding auth front end

---
+++ frontend/src/components/PeopleYouMayKnow.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="p-4 bg-white border border-gray-200 rounded-lg">
+        <h3 class="mb-6 text-xl">People you may know</h3>
+            <div class="space-y-4">
+                <div class="flex items-center justify-between">
+                    <div class="flex itemd-center space-x-2">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh2ibh9YBLzRs29v8VE96dT1ow0TTfys_gAw&usqp=CAU" class="w-[40px] rounded-full">
+                        <p class="text-xs"><strong>Code with Wenxuan</strong></p>
+                    </div>
+                    <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">show</a>
+                </div>
+            </div>
+    </div>
+</template>
