b9e2b269b10a9e63d02bf23d25efa5278b16e3d0
Wenxuan Lu <hughjin@Hughs-MacBook-Pro.local>
2023-08-25 20:19:09 -0700

    commit

--- backend/chat/api.py
+++ backend/chat/api.py
@@ -9,3 +9,10 @@ def conversation_list(request):
     conversations = Conversation.objects.filter(users__in=list([request.user]))
     serializer = ConversationSerializer(conversations, many=True)
     return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET'])
+def conversation_detail(request, pk):
+    conversation = Conversation.objects.filter(users__in=list([request.user])).get(pk=pk)
+    serializer = ConversationDetailSerializer(conversation)
+
+    return JsonResponse(serializer.data, safe=False)
